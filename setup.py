05eb5d2974b673d724bd05bfce3680889e777a0f
Andrew White <white.d.andrew@gmail.com>
2021-08-14 13:25:34 -0400

    Added publish workflow

--- setup.py
+++ setup.py
@@ -4,16 +4,29 @@ from setuptools import setup
 
 exec(open("exmol/version.py").read())
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="exmol",
     version=__version__,
     description="Counterfactual generation with STONED SELFIES",
     author="Aditi Seshadri, Geemi Wellawatte, Andrew White",
     author_email="andrew.white@rochester.edu",
-    url="github.com/ur-whitelab/exmol",
+    url="https://ur-whitelab.github.io/exmol/",
     license="MIT",
     packages=["exmol", "exmol.stoned"],
-    install_requires=["selfies", "numpy", "rdkit-pypi", "matplotlib", "scikit-learn"],
+    install_requires=["selfies", "numpy",
+                      "rdkit-pypi", "matplotlib", "scikit-learn"],
     test_suite="tests",
-    zip_safe=True,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+        classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Chemistry",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Typing :: Typed"
+    ]
 )
