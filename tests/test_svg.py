aeba985715b95039078d35af8cd0bbff79acc4b7
geemi725 <gwellawa@ur.rochester.edu>
2021-10-22 08:49:40 -0400

    GNN save model commented out

--- tests/test_svg.py
+++ tests/test_svg.py
@@ -15,7 +15,7 @@ def test_insert_svg():
     exmol.plot_cf(exps)
     exmol.plot_space(samples, exps)
     svg = exmol.insert_svg(exps)
-    with open('test-space.svg', 'w') as f:
+    with open("test-space.svg", "w") as f:
         f.write(svg)
 
 
@@ -24,12 +24,13 @@ def test_insert_svg_long():
         return int("N" in s)
 
     samples = exmol.sample_space(
-        "O=C(COCCCCCCCCCC)CC(C)C1CCCC1C2=CC=CC(F)=C2 ", model, batched=False)
+        "O=C(COCCCCCCCCCC)CC(C)C1CCCC1C2=CC=CC(F)=C2 ", model, batched=False
+    )
     exps = exmol.cf_explain(samples, 3)
     exmol.plot_cf(exps)
     exmol.plot_space(samples, exps, mol_size=(300, 200))
     svg = exmol.insert_svg(exps, mol_size=(300, 200))
-    with open('test-long.svg', 'w') as f:
+    with open("test-long.svg", "w") as f:
         f.write(svg)
 
 
@@ -38,9 +39,10 @@ def test_insert_svg_grid():
         return int("N" in s)
 
     samples = exmol.sample_space(
-        "O=C(COCCCCCCCCCC)CC(C)C1CCCC1C2=CC=CC(F)=C2 ", model, batched=False)
+        "O=C(COCCCCCCCCCC)CC(C)C1CCCC1C2=CC=CC(F)=C2 ", model, batched=False
+    )
     exps = exmol.cf_explain(samples, 3)
     exmol.plot_cf(exps)
     svg = exmol.insert_svg(exps)
-    with open('test-cf.svg', 'w') as f:
+    with open("test-cf.svg", "w") as f:
         f.write(svg)
